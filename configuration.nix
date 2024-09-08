70b116043afa91c39294ce83579a1721cbe9c0fe
Erlangga Yudi Pradana <erlanggayudipradana@gmail.com>
2024-09-08 21:31:33 +0700

    chore!: update configuration.nix
    - Add system packages
    - Add fonts
    - Configure picom settings
    - Add Xorg driver and device section

--- configuration.nix
+++ configuration.nix
@@ -1,152 +1,161 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  # Import hardware configuration
+  imports = [ /etc/nixos/hardware-configuration.nix ];
 
-  # Bootloader.
+  # Bootloader Configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
+  boot.blacklistedKernelModules = [ "snd_pcsp" "pcspkr" ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Hostname
+  networking.hostName = "nixos";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # Enable NetworkManager for networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Timezone and Locale Settings
   time.timeZone = "Asia/Jakarta";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
+  # X11 Configuration and Window Manager
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = false;
-  # services.xserver.desktopManager.gnome.enable = false;
-
   services.xserver.displayManager.lightdm.enable = true;
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "rlggyp";
 
   services.xserver.windowManager.awesome = {
-		enable = true;
+    enable = true;
     luaModules = with pkgs.luaPackages; [
-      luarocks # is the package manager for Lua modules
-      vicious
-    	luadbi-mysql # Database abstraction layer
+      luarocks    # Lua package manager
+      vicious     # Awesome WM widget library
+      luadbi-mysql # Database abstraction layer for MySQL
     ];
   };
+  services.xserver.videoDrivers = [ "intel" ];
 
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
+  # X11 Device Configuration
+  services.xserver.deviceSection = ''
+    Option "DRI" "3"
+    Option "TearFree"        "false"
+    Option "TripleBuffer"    "false"
+    Option "SwapbuffersWait" "false"
+  '';
+
+  # Enable touchpad and input settings via libinput
+  services.libinput = {
+    enable = true;
+    touchpad.naturalScrolling = true;
+    mouse.disableWhileTyping = true;
+    touchpad.disableWhileTyping = true;
+    touchpad.tappingButtonMap = "lrm";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # Compositor Configuration (Picom)
+	services.picom = {
+    enable = true;
+		settings =  {
+		  backend = "glx";
+		  vSync = true;
+			blur = false;
+			blurExclude = [ ];
+    };
+		backend = "glx";
+		vSync = true;
+    fade = true;
+    fadeDelta = 4;
+    shadow = false;
+    shadowExclude = [ ];
+    inactiveOpacity = 1;
+  };
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+  # Hardware Acceleration and Firmware
+  hardware.opengl.enable = true;
+  hardware.opengl.extraPackages = with pkgs; [ intel-media-driver ];
+  hardware.enableAllFirmware = true;
+
+  # Sound with Pipewire
+  sound.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = true;
   };
+  security.rtkit.enable = true;
 
-  services.picom.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput = {
-    enable = true;
-		mouse.naturalScrolling = true;
-		mouse.tappingButtonMap = "lrm";
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User Configuration
   users.users.rlggyp = {
     isNormalUser = true;
-    description = "r";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    description = "erlangga";
+    extraGroups = [ "users" "networkmanager" "wheel" "docker" "audio" "video" "dialout" ];
+    packages = with pkgs; [ ];
     shell = pkgs.zsh;
   };
 
+  # Docker
   virtualisation.docker.enable = true;
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-	programs.zsh.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Enable Zsh
+  programs.zsh.enable = true;
+
+  # System Packages
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-    pkgs.neovim
-    pkgs.git
-    pkgs.gnumake
-    pkgs.cmake
-    pkgs.gcc
-    pkgs.kitty
-    pkgs.tmux
-    pkgs.zsh
+    neovim
+    firefox
+    chromium
+    git
+    picom
+    eza
+    ripgrep
+    btop
+    nodejs
+    bat
+    pcmanfm
+    guvcview
+    mpv
+    gnumake
+    cmake
+    gcc
+    kitty
+    tmux
+    zsh
+    dmenu
+    brightnessctl
+    lxappearance
+    breeze-gtk
+    v4l-utils
+    zip
+    unzip
+    unrar
+    xclip
+    breeze-icons
+    pavucontrol
+    pamixer
+    sxiv
+    feh
+    yazi
+    rofimoji
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Fonts
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    nerdfonts
+  ];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+  # System State Version
+  system.stateVersion = "24.05";
 }
